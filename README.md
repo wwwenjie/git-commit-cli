c896ff24159ac3839ed982483fa3d1c27e8870d9
wenjie <jinwenjie@live.com>
2021-11-18 22:30:41 +0800

    [wenjie] #N/A feat: init project

---
+++ README.md
@@ -0,0 +1,33 @@
+# git-commit-cli
+
+## About
+
+This is a git commit cli enforce the git user to write better git commit messages easily.
+
+## Requirements
+
+- macOS
+
+The shells only tests in macOS terminal, may need some changes on other platforms
+
+## Install
+
+```
+bash -c "$(curl -sS URL)"
+```
+
+## Usage
+
+- `commit`: add **all** files to git stage and commit
+
+- `commit --clean`: clean commit history
+
+- `commit --config`: set commit message username
+
+- `commit --update`: update git-commit-cli
+
+- `commit --uninstall`: uninstall git-commit-cli
+
+## Contribute
+
+Any contribution is welcome!
---
+++ install.sh
@@ -0,0 +1,33 @@
+SHELL_LOCATION="/usr/local/bin/commit"
+SHELL_REMOTE_URL=""
+
+ESC=$(printf "\033")
+RED=$ESC[31m
+GREEN=$ESC[32m
+RESET=$ESC[0m
+
+function setDownloadShellCheckSum {
+    checkSum=$(cat $SHELL_LOCATION | shasum -a 256)
+
+    newFileCheckSum="FILE_CHECKSUM=\"${checkSum}\""
+
+    sed -i "" "s/^FILE_CHECKSUM=.*/${newFileCheckSum}/" $SHELL_LOCATION
+}
+
+echo "Fetching data..."
+
+curl -sS $SHELL_REMOTE_URL >$SHELL_LOCATION
+
+setDownloadShellCheckSum
+
+clear
+
+chmod +x $SHELL_LOCATION
+
+commit --config
+
+echo
+echo "${RESET}Run ${GREEN}commit${RESET} in any git repository to commit easy!"
+
+echo
+echo "Run ${GREEN}commit --help${RESET} to get more info"
